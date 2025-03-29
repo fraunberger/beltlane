93ecdfe218e571ad16126dc8a49b13afca36bf8b
Michael Fraunberger <michaelfraunberger@MacBookAir.attlocal.net>
2025-03-29 11:39:15 -0400

    tighten margins

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,13 @@ import About from "@/components/About";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-6">
+    <main className="flex min-h-screen flex-col items-center justify-center p-2">
       <Image
         src="/logo2.png"
         alt="Beltlane Logo"
         width={200}
         height={100}
-        className="mb-6"
+        className="mb-2"
       />
       <TallyForm />
       <About />
