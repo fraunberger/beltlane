528a5a8402d3151513b5ad809f3f37a7aa22e9c6
Michael Fraunberger <michaelfraunberger@MacBookAir.attlocal.net>
2025-03-29 10:22:22 -0400

    integrate tally form

---
+++ src/components/TallyForm.tsx
@@ -0,0 +1,16 @@
+export default function TallyForm() {
+  return (
+    <div className="w-full max-w-3xl mx-auto my-8">
+      <iframe
+        data-tally-src="https://tally.so/r/n946K1"
+        width="100%"
+        height="800"
+        frameBorder="0"
+        marginHeight={0}
+        marginWidth={0}
+        title="Petition Form"
+      ></iframe>
+      <script async src="https://tally.so/widgets/embed.js"></script>
+    </div>
+  );
+}
