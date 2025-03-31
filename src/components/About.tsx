e732f5dabe90993a4a6dee485a35efd25b5b1b8a
Michael Fraunberger <michaelfraunberger@MacBookAir.attlocal.net>
2025-03-31 18:57:34 -0400

    dark mode

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,7 +2,7 @@
 
 export default function About() {
   return (
-    <section className="max-w-3xl mx-auto my-10 p-6 bg-gray-100 rounded-lg">
+    <section className="max-w-3xl mx-auto my-10 p-6 rounded-lg" style={{ background: 'var(--card-background)', color: 'var(--card-foreground)' }}>
       <h2 className="text-2xl font-bold mb-3">About Us</h2>
       <p className="text-base">
         The Beltlane project is a a single-purpose advocacy group that seeks to generate and communicate support for the Beltlane concept.
